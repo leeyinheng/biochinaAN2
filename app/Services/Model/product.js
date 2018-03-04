745fcf059e127b6656bc93a1097eb4f7ed505979
Henry Lee <leeyinheng@yahoo.com>
2018-03-04 14:12:48 +0800

    china.tw html5

--- app/Services/Model/product.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Product = (function () {
-    function Product() {
-        this.Promotion = false;
-    }
-    return Product;
-}());
-exports.Product = Product;
-//# sourceMappingURL=product.js.map
